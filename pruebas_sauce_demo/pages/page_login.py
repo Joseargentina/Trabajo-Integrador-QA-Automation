e0367e579564f70025bddc537ed26ffd82089a30
Joseargentina <josebarone87@hotmail.com>
2024-10-14 17:24:25 -0300

    configurando integracion continua

--- pruebas_sauce_demo/pages/page_login.py
+++ pruebas_sauce_demo/pages/page_login.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Page_Login():
     def __init__(self, driver):
@@ -8,6 +10,10 @@ class Page_Login():
         self.login_button = (By.ID,'login-button')
     
     def login(self,usuario,password):
+        # Espera hasta que el campo de nombre de usuario sea visible
+        WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located(self.user_name)
+            )
         self.driver.find_element(*self.user_name).send_keys(usuario)
         self.driver.find_element(*self.password).send_keys(password)
         self.driver.find_element(*self.login_button).click()

4be06df74f76cbe7135a183b6c52454b946401c8
Joseargentina <josebarone87@hotmail.com>
2024-10-14 18:00:05 -0300

    probando el funcinamiento correcto de la  integracion continua

--- html_runner.py
+++ html_runner.py
@@ -11,10 +11,10 @@ except Exception as e:
     
 # Cargar y ejecutar pruebas de API
 
-# pruebas_api_suite = unittest.TestLoader().discover('pruebas_poke_api/tests')
-# runner_api = HTMLTestRunner(output='report_directory/api_report', report_title='Pruebas Poke API', combine_reports=True)
-# try:
-#     runner_api.run(pruebas_api_suite)
-# except Exception as e:
-#     print(f'Ocurrió algún problema: {e}')
+pruebas_api_suite = unittest.TestLoader().discover('pruebas_poke_api/tests')
+runner_api = HTMLTestRunner(output='report_directory/api_report', report_title='Pruebas Poke API', combine_reports=True)
+try:
+    runner_api.run(pruebas_api_suite)
+except Exception as e:
+    print(f'Ocurrió algún problema: {e}')
 

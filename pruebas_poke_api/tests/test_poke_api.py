3fa7f81f0509c7fe133ec24f3d1d6f3b3a2c2223
Joseargentina <josebarone87@hotmail.com>
2024-10-13 21:28:03 -0300

    termine las pruebas de la api

--- pruebas_poke_api/tests/test_poke_api.py
+++ pruebas_poke_api/tests/test_poke_api.py
@@ -15,11 +15,11 @@ class TestPoke(unittest.TestCase):
         self.assertEqual(r.status_code, 200, "La solicitud no fue exitosa")
         data = r.json()
         
-        self.assertIn('size', data)  # Asegúrate de que la clave 'size' esté presente
+        self.assertIn('size', data, "No se encontró 'size' en los datos de la respuesta")
         self.assertEqual(data['size'], 20, "El tamaño no es igual a 20")
-        self.assertIn('soil_dryness', data)
+        self.assertIn('soil_dryness', data, "No se encontró 'soil_dryness' en los datos de la respuesta")
         self.assertEqual(data['soil_dryness'], 15)
-        self.assertIn('firmness', data)
+        self.assertIn('firmness', data, "No se encontró 'firmness' en los datos de la respuesta")
         self.assertIn('name', data['firmness'])
         self.assertEqual(data['firmness']['name'],'soft')
     
@@ -27,20 +27,21 @@ class TestPoke(unittest.TestCase):
         r = requests.get(self.url + 'berry/2')
         self.assertEqual(r.status_code, 200, "La solicitud no fue exitosa")
         data = r.json()
-        self.assertIn('firmness', data)
-        self.assertIn('name', data['firmness'])
+        self.assertIn('firmness', data, "No se encontró 'firmness' en los datos de la respuesta")
+        self.assertIn('name', data['firmness'], "No se encontró 'name' en los datos de la firmness")
         self.assertEqual(data['firmness']['name'], 'super-hard')
         self.assertTrue(data['size'] > 20, "El tamaño no es mayor a 20")
         self.assertEqual(data['soil_dryness'], 15)
         
     def test_pikachu_base_experience_and_type(self):
         r = requests.get(self.url + 'pokemon/pikachu/')
-        print(f'La url es: {self.url + 'pokemon/pikachu/'}')
+        print(f'La url es: {self.url + "pokemon/pikachu/"}')
         self.assertEqual(r.status_code, 200, "La solicitud no fue exitosa")
         data = r.json()
-        self.assertIn('base_experience', data)
+        self.assertIn('base_experience', data, "No se encontró 'base_experience' en los datos de la respuesta")
         self.assertTrue(10 < data['base_experience'] < 1000, "La experiencia base no está en el rango esperado")
+        self.assertIn('types', data,"No se encontró 'types' en los datos de la respuesta")
         self.assertEqual(data['types'][0]['type']['name'],'electric',"Pikachu no es de tipo electric")
         
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
